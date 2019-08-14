ded33bda1bfa9f3535deca096b3c1881d335fe79
alexander-iv <alexander.an.ivanov@gmail.com>
2019-08-14 18:06:25 +1000

    added spring WebApplicationInitializer-s

---
+++ hw13-di/di-service/src/main/java/alexander/ivanov/WebInitializer.java
@@ -0,0 +1,27 @@
+package alexander.ivanov;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return super.getServletFilters();
+    }
+}
---
+++ hw13-di/di-service/src/main/java/alexander/ivanov/controller/UserController.java
@@ -0,0 +1,16 @@
+package alexander.ivanov.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping("/test")
+    public void test() {
+        logger.info("test");
+    }
+}
